214daf3e8db265f1130d402754a653a6ea8473fc
Adriano <adrianorsimoes@gmail.com>
2021-03-09 13:22:50 +0100

    Continuos mode.

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 
 func handleErr(err error) {
 	if err != nil {
-		//fmt.Println(err)
+		fmt.Println(err)
 	}
 }
 
@@ -22,16 +22,18 @@ func main() {
 	err = flowClient.Ping(context.Background())
 	handleErr(err)
 
-	// fetch latest block
-	latestBlock, err := flowClient.GetLatestBlock(context.Background(), false)
-	handleErr(err)
-	fmt.Println("current height: ", latestBlock.Height)
+	for {
+		// fetch latest block
+		latestBlock, err := flowClient.GetLatestBlock(context.Background(), false)
+		handleErr(err)
+		fmt.Println("current height: ", latestBlock.Height)
 
-	blockSize := 20
-	for i := 0; i <= 160; i+=blockSize {
-		fmt.Println("current block: ", int64(latestBlock.Height) - int64(i))
-		fetchBlocks(flowClient, int64(latestBlock.Height) - int64(i) - int64(blockSize), int64(latestBlock.Height) - int64(i), "A.c1e4f4f4c4257510.Market.MomentListed")
-		fetchBlocks(flowClient, int64(latestBlock.Height) - int64(i) - int64(blockSize), int64(latestBlock.Height) - int64(i), "A.c1e4f4f4c4257510.Market.MomentPriceChanged")
+		blockSize := 20
+		for i := 0; i < 20; i+=blockSize {
+			//fmt.Println("current block: ", int64(latestBlock.Height) - int64(i))
+			fetchBlocks(flowClient, int64(latestBlock.Height) - int64(i) - int64(blockSize), int64(latestBlock.Height) - int64(i), "A.c1e4f4f4c4257510.Market.MomentListed")
+			fetchBlocks(flowClient, int64(latestBlock.Height) - int64(i) - int64(blockSize), int64(latestBlock.Height) - int64(i), "A.c1e4f4f4c4257510.Market.MomentPriceChanged")
+		}
 	}
 }
 
@@ -52,7 +54,7 @@ func fetchBlocks(flowClient *client.Client, startBlock int64, endBlock int64, ty
 			if(e.Price() < 35){
 				saleMoment, err := topshot.GetSaleMomentFromOwnerAtBlock(flowClient, blockEvent.Height, *e.Seller(), e.Id())
 				handleErr(err)
-				if(e.Price()< 9 || (saleMoment != nil && (saleMoment.SerialNumber() <= 500 || (e.Price() < 25 && saleMoment.SerialNumber() <= 1000)))) {
+				if(e.Price()<= 9 || (saleMoment != nil && (saleMoment.SerialNumber() <= 500 || (e.Price() < 25 && saleMoment.SerialNumber() <= 1000)))) {
 					fmt.Println(saleMoment, "\tPrice: ", e.Price())
 				}
 			}
