38f04a210af62c6ff1f45e50474b61e4ba001ea9
Nuno Monteiro <nuno.monteir@gmail.com>
2021-03-11 14:25:58 +0000

    Added "-unique" flag to run in a mode where: -entries only get shown once -the screen gets cleared every time the list updates -the alert is sent only once per new item

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"os"
 	"os/exec"
 	"context"
@@ -19,6 +20,24 @@ func handleErr(err error) {
 }
 
 func main() {
+
+	boolPtr := flag.Bool("unique",false,"whether entries are merged till they're unique or not")
+	flag.Parse()
+
+	if (*boolPtr) {
+		fmt.Println("using unique mode!")
+	} else {
+		fmt.Println("NOT using unique mode!")
+	}
+
+	if (*boolPtr) {
+		uniqueModeLogic()
+	} else {
+		nonUniqueModeLogic()
+	}
+}
+
+func nonUniqueModeLogic() {
 	// connect to flow
 	flowClient, err := client.New("access.mainnet.nodes.onflow.org:9000", grpc.WithInsecure())
 	handleErr(err)
@@ -28,10 +47,8 @@ func main() {
 	// Run a bigger fetch block the first time, to check more blocks in the past:
 	latestBlock, err := flowClient.GetLatestBlock(context.Background(), false)
 	handleErr(err)
-	fetchBlocks(flowClient, int64(latestBlock.Height - 50), int64(latestBlock.Height), "A.c1e4f4f4c4257510.Market.MomentListed")
 
-	// blocks :=fetchFilteredBlocks(flowClient, int64(latestBlock.Height - 50), int64(latestBlock.Height), "A.c1e4f4f4c4257510.Market.MomentListed")
-	// dumpMoments(blocks)
+	fetchBlocks(flowClient, int64(latestBlock.Height - 50), int64(latestBlock.Height), "A.c1e4f4f4c4257510.Market.MomentListed")
 
 	for {
 		// fetch latest block
@@ -40,14 +57,64 @@ func main() {
 		//fmt.Println("current height: ", latestBlock.Height)
 
 		blockSize := 10
+		
 		for i := 0; i < blockSize; i+=blockSize {
 			//fmt.Println("current block: ", int64(latestBlock.Height) - int64(i))
 			
 			fetchBlocks(flowClient, int64(latestBlock.Height) - int64(i) - int64(blockSize), int64(latestBlock.Height) - int64(i), "A.c1e4f4f4c4257510.Market.MomentListed")
 			//fetchBlocks(flowClient, int64(latestBlock.Height) - int64(i) - int64(blockSize), int64(latestBlock.Height) - int64(i), "A.c1e4f4f4c4257510.Market.MomentPriceChanged")
+		}
+		fmt.Print(".")
+	}
+}
 
-			//blocks = fetchFilteredBlocks(flowClient, int64(latestBlock.Height) - int64(i) - int64(blockSize), int64(latestBlock.Height) - int64(i), "A.c1e4f4f4c4257510.Market.MomentListed")
-			//dumpMoments(blocks)
+func uniqueModeLogic() {
+	// connect to flow
+	flowClient, err := client.New("access.mainnet.nodes.onflow.org:9000", grpc.WithInsecure())
+	handleErr(err)
+	err = flowClient.Ping(context.Background())
+	handleErr(err)
+	
+	// Run a bigger fetch block the first time, to check more blocks in the past:
+	latestBlock, err := flowClient.GetLatestBlock(context.Background(), false)
+	handleErr(err)
+	
+	filteredMoments := make([]*topshot.SaleMoment,0)
+	momentCount := 0
+
+	blocks :=fetchFilteredBlocks(flowClient, int64(latestBlock.Height - 50), int64(latestBlock.Height), "A.c1e4f4f4c4257510.Market.MomentListed")
+	filteredMoments = mergeUniqueBlocks(blocks, filteredMoments)
+
+	if (len(filteredMoments) > momentCount) {
+		dumpMoments(filteredMoments)	
+		momentCount = len(filteredMoments)
+	}
+
+	for {
+		// fetch latest block
+		latestBlock, err := flowClient.GetLatestBlock(context.Background(), false)
+		handleErr(err)
+		//fmt.Println("current height: ", latestBlock.Height)
+
+		blockSize := 10
+		
+		for i := 0; i < blockSize; i+=blockSize {
+			//fmt.Println("current block: ", int64(latestBlock.Height) - int64(i))
+			
+			//fetchBlocks(flowClient, int64(latestBlock.Height) - int64(i) - int64(blockSize), int64(latestBlock.Height) - int64(i), "A.c1e4f4f4c4257510.Market.MomentListed")
+			//fetchBlocks(flowClient, int64(latestBlock.Height) - int64(i) - int64(blockSize), int64(latestBlock.Height) - int64(i), "A.c1e4f4f4c4257510.Market.MomentPriceChanged")
+
+			blocks = fetchFilteredBlocks(flowClient, int64(latestBlock.Height) - int64(i) - int64(blockSize), int64(latestBlock.Height) - int64(i), "A.c1e4f4f4c4257510.Market.MomentListed")
+
+			filteredMoments = mergeUniqueBlocks(blocks, filteredMoments)
+
+			// blocks = fetchFilteredBlocks(flowClient, int64(latestBlock.Height) - int64(i) - int64(blockSize), int64(latestBlock.Height) - int64(i), "A.c1e4f4f4c4257510.Market.MomentPurchased")
+			// filteredMoments = removePurchasedBlocks(blocks, filteredMoments)	
+
+			if (len(filteredMoments) > momentCount) {
+				dumpMoments(filteredMoments)	
+				momentCount = len(filteredMoments)
+			}
 		}
 		fmt.Print(".")
 	}
@@ -94,7 +161,7 @@ func fetchBlocks(flowClient *client.Client, startBlock int64, endBlock int64, ty
 }
 
 func shouldPrintPlayer(moment topshot.MomentListed, sale *topshot.SaleMoment) bool {
-	if(moment.Price() < 5){
+	if(moment.Price() < 6){
 		return true;
 	}
 	
@@ -114,7 +181,7 @@ func shouldPrintPlayer(moment topshot.MomentListed, sale *topshot.SaleMoment) bo
 		return true;	
 	}
 	
-	if(moment.Price() < 10 && sale.NumMoments() <= 15000){
+	if(moment.Price() < 15 && sale.NumMoments() <= 15000 && sale.SerialNumber() <= 2500){
 		return true;	
 	}
 	
@@ -191,8 +258,11 @@ func clearScreen() {
 
 func dumpMoments(moments []*topshot.SaleMoment) {
 	//first clear
-	//clearScreen()
+	clearScreen()
 
+	//new list needs to warn
+	fmt.Println("\a")
+	fmt.Println("---------------------------------------------")
 	for _, saleMoment := range moments {
 		
 		c := color.New(color.FgWhite)
@@ -210,4 +280,54 @@ func dumpMoments(moments []*topshot.SaleMoment) {
 		c.Println(saleMoment, "\tPrice: ", fmt.Sprintf("%.0f", saleMoment.Price()))	
 		fmt.Println("")
 	}
+	fmt.Println("++++++++++++++++++++++++++++++++++++++++++++++")
+}
+
+func mergeUniqueBlocks(blocks []*topshot.SaleMoment, filteredMoments []*topshot.SaleMoment) []*topshot.SaleMoment {
+	mergedMoments := make([]*topshot.SaleMoment, 0)
+	mergedMoments = append(mergedMoments,filteredMoments...)
+
+	for _, newMoment := range blocks {
+		found := false
+		for _, existingMoment := range filteredMoments {
+			if (existingMoment.SetID() == newMoment.SetID() &&
+				existingMoment.PlayID() == newMoment.PlayID() &&
+				existingMoment.Price() == newMoment.Price() &&
+				existingMoment.SerialNumber() == newMoment.SerialNumber()) {
+					found = true
+					break
+				} 
+		}
+
+		if (found == false) {
+			mergedMoments = append(mergedMoments, newMoment)
+		}
+	}
+
+	return mergedMoments
+}
+
+func removePurchasedBlocks(blocks []*topshot.SaleMoment, filteredMoments []*topshot.SaleMoment) []*topshot.SaleMoment {
+	mergedMoments := make([]*topshot.SaleMoment, 0)
+
+	for _, existingMoment := range filteredMoments {
+		found := false
+
+		for _, goneMoment := range blocks {
+			
+		
+			if (existingMoment.SetID() == goneMoment.SetID() &&
+				existingMoment.PlayID() == goneMoment.PlayID() &&
+				existingMoment.Price() == goneMoment.Price()) {
+					found = true
+					break
+			} 
+		}
+
+		if (found == false) {
+			mergedMoments = append(mergedMoments, existingMoment)
+		}
+	}
+
+	return mergedMoments
 }
